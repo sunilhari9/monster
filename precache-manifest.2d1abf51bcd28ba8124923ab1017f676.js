2bf4feca7026062215cb0190c422536763d0e4b9
Suneel Manyam <sunilhari9@gmail.com>
2020-10-23 14:40:38 +0530

    Updates

---
+++ precache-manifest.2d1abf51bcd28ba8124923ab1017f676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c618536413a04d00bbf33920f7a2a0b",
+    "url": "/moster/index.html"
+  },
+  {
+    "revision": "45bb6f763b12e8ac2d5e",
+    "url": "/moster/static/css/main.d721cc66.chunk.css"
+  },
+  {
+    "revision": "41f8b8672aad5322edd3",
+    "url": "/moster/static/js/2.e88ff5fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/moster/static/js/2.e88ff5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45bb6f763b12e8ac2d5e",
+    "url": "/moster/static/js/main.75f95e9e.chunk.js"
+  },
+  {
+    "revision": "4a27d00ddccc5a6d343c",
+    "url": "/moster/static/js/runtime-main.b4210268.js"
+  }
+]);
